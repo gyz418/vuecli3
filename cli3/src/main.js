8032ff617f8cd3c0d5afca81f1907fe21ee5c32d
kangjiawei <kangjiawei@mizhua.com>
2019-04-22 17:40:30 +0800

    render 函数

--- cli3/src/main.js
+++ cli3/src/main.js
@@ -1,19 +1,39 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import Bus from './tools/bus'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import Bus from './tools/bus';
 // import MintUI from 'mint-ui'    // 这里不用引 mint-ui
-import '@/assets/mintui.css'
+import '@/assets/mintui.css';
+import emit from './views/emit/father'  // 引父组件
+import emitson from './views/emit/son'  // 引父组件
 
-if(process.env.NODE_ENV==='development') require('./mock/mock')   // 开发环境引入 mock.js
+if (process.env.NODE_ENV === 'development') require('./mock/mock');   // 开发环境引入 mock.js
 
-Vue.prototype.$Bus=Bus
-Vue.config.productionTip = false
+Vue.prototype.$Bus = Bus;
+Vue.config.productionTip = false;
 // Vue.use(MintUI)
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  // render: h => h(App)
+  // render:h=>h(emit)
+  render:h=>h(emitson,{
+    props:{  // 传 props
+      value:'haha'
+    }
+  })
+/*
+//  渲染div
+  render: h => {
+    return h('div', {
+      attrs: {
+        id: 'box',
+      },
+      style: {
+        color: 'red'
+      }
+    },'haha');
+  }*/
+}).$mount('#app');
