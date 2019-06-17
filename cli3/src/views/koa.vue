67a2dc06570dc57d840baa8d8d944622a7c544de
kangjiawei <kangjiawei@mizhua.com>
2019-06-17 13:55:08 +0800

    koa

--- cli3/src/views/koa.vue
+++ cli3/src/views/koa.vue
@@ -4,7 +4,7 @@
       <div class="list" v-for="(val,key) in list" :key="key">
         {{val.id+': '+val.name}}
       </div>
-      {{player.nickname}}
+      {{player.id}}
     </div>
 </template>
 
@@ -24,7 +24,17 @@
     },
     methods: {
       get(){
-        axios.get(this.url+'/getUser').then(res=>{
+        let token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiMTIzIiwiaWF0IjoxNTYwNDc3Mzg0LCJleHAiOjE1NjA0OTE3ODR9.XZMuAStEi0co9pYxCq5hmqdoq-faVqqGLCweIBETZF4'
+        axios({
+          method:'get',
+          url:this.url+'/result',
+          // url:this.url+'/login',
+          // url:this.url+'/list',
+          headers:{'Authorization':token}
+        }).then(res=>{
+        // axios.get(this.url+'/result',{headers:{'authorization':token}}).then(res=>{
+        // axios.get(this.url+'/list').then(res=>{
+        // axios.post(this.url+'/getParam').then(res=>{
           console.log('res',res.data);
           // this.list=res.data
           this.player=res.data
